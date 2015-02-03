a76888fa72d85bc10dfcc7efeef8c0a5611d6088
Arindam Nandi <arindamn@buffalo.edu>
2015-02-02 22:25:42 -0500

    Changed group name

--- kern/startup/main.c
+++ kern/startup/main.c
@@ -100,7 +100,7 @@ boot(void)
 	kprintf("%s", harvard_copyright);
 	kprintf("\n");
 
-	kprintf("Put-your-group-name-here's system version %s (%s #%d)\n", 
+	kprintf("Kernel Panic's system version %s (%s #%d)\n", 
 		GROUP_VERSION, buildconfig, buildversion);
 	kprintf("\n");
 
